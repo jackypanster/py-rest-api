d68976c1cc33dcc0d6c6462a5972d98bb913a8bc
jackypan <87015598@qq.com>
2018-02-06 17:55:19 +0800

    disable logging

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ app = Sanic()
 @app.route("/api")
 async def test(request):
     return json({"hello": "world"})
+
+if __name__ == "__main__":
+    # disable debug messages
+    app.run(host='0.0.0.0', port=8000, workers=4,
+            debug=False, access_log=False)
